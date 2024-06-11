8ca147d02497b2dfdc6337af06e99db2ed065de4
Sardor Sharipov <sardorsharipov2000@mail.ru>
2024-06-11 20:46:44 +0300

    udpate main

--- main.py
+++ main.py
@@ -100,8 +100,11 @@ def check_com_port(port: str):
         try:
             if not ser.is_open:
                 ser.open()
-            # if ser.in_waiting > 0:
-            data = ser.readline()#.decode('utf-8').strip()
+            for _ in range(1000):
+                if ser.in_waiting > 0:
+                    print('a')
+                    break
+            data = ser.readline().decode('utf-8').strip()
             hex_string = data.strip()
             if len(hex_string) >= 54:
                 target_hex = hex_string[46:54]
