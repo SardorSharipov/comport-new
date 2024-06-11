f4ec138953b569d20311779c1a078fe911560fb2
Sardor Sharipov <sardorsharipov2000@mail.ru>
2024-06-11 22:02:22 +0300

    udpate main

--- ports.py
+++ ports.py
@@ -6,4 +6,4 @@ for port in sorted(ports):
     if '/dev/ttyUSB' in port.device:
         print(f'COMPORT: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}', port)
     else:
-        print(f'OTHER: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}'', port)
+        print(f'OTHER: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}', port)
