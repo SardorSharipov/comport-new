0765ce656925be000f3f78cc314acf6342ccd520
Sardor Sharipov <sardorsharipov2000@mail.ru>
2024-06-11 22:01:54 +0300

    udpate main

--- ports.py
+++ ports.py
@@ -4,6 +4,6 @@ ports = serial.tools.list_ports.comports()
 
 for port in sorted(ports):
     if '/dev/ttyUSB' in port.device:
-        print(f'COMPORT: NAME={port.device}, LOCATION={port.location}', port)
+        print(f'COMPORT: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}', port)
     else:
-        print(f'OTHER: NAME={port.device}, LOCATION={port.location}', port)
+        print(f'OTHER: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}'', port)
