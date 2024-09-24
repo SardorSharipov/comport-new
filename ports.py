ab69351b0348990e671daf2e52ce013bc609c5d1
Sardor Sharipov <sardorsharipov2000@mail.ru>
2024-09-24 09:08:40 +0300

    add same vid/pid

--- ports.py
+++ ports.py
@@ -5,6 +5,6 @@ ports = serial.tools.list_ports.comports()
 for port in sorted(ports):
     if '/dev/ttyUSB' in port.device:
         print(f'COMPORT: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}')
-        print(port)
+        print(port, port.serial_number, port.hwid)
     else:
         print(f'OTHER: NAME={port.device}, LOCATION={port.location}, VID:PID={port.vid}:{port.pid}')
